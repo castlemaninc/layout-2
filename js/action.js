ed4df28e7fe93e7b5f90abc66c74c0a38e622765
Bryan Castleman <castlemaninc@gmail.com>
2018-04-05 10:09:56 -0700

    removed background colors from breakpoints and styled social links

--- js/action.js
+++ js/action.js
@@ -1,5 +1,6 @@
 $( document ).ready(function() {
 
+	// Smooth scrolling to the top of the webpage when 'Back to Top link is clicked'
 	$('footer a').click(function(e) {
 	  	console.log('footer a clicked');
 	    
